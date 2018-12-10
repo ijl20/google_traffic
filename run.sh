299b9bf6ca1678b7f65ee1a4fd77544895769a4f
Ian Lewis <ijl20@cam.ac.uk>
2018-12-10 14:07:53 +0000

    debugging run.sh

--- run.sh
+++ run.sh
@@ -7,10 +7,14 @@ declare -a boxes=("cambridge"
 
 # get script directory
 
-IMG_DIR="/media/tfc/google_traffic_map/"
+IMG_DIR="/media/tfc/google_traffic_map"
+
+echo Writing image files to "${IMG_DIR}"
 #IMG_DIR="/home/ijl20/google_traffic_data"
 
 EXEC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo EXEC_DIR=${EXEC_DIR}
 
 # iterate the boxes
 
@@ -19,8 +23,8 @@ do
     echo $config_id
     CONFIG_SH="${EXEC_DIR}/config/${config_id}.sh"
     source "${CONFIG_SH}"
-    echo ${box_title} # from config file
-    "${EXEC_DIR}/capture_map.sh" "'@${google_lat},${google_lng},${google_zoom}/data=!5m1!1e1'" "${IMG_DIR}/${box_id}"
+    echo box_title- ${box_title} # from config file
+    echo command- "${EXEC_DIR}/capture_map.sh" "'@${google_lat},${google_lng},${google_zoom}/data=!5m1!1e1'" "${IMG_DIR}/${box_id}"
 done
 
 #/home/ijl20/google_traffic/capture_map.sh  '@52.2122231,0.1196616,13z/data=!5m1!1e1' /data/tfc/google_traffic_map/      cambridge
