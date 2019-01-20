079d47055419cea39efc2f6fea5a2f0fa8c01d01
Ian Lewis <ijl20@cam.ac.uk>
2019-01-20 13:54:46 +0000

    bugfix use double quotes in run.sh

--- run.sh
+++ run.sh
@@ -22,9 +22,16 @@ for config_id in "${boxes[@]}"
 do
     echo $config_id
     CONFIG_SH="${EXEC_DIR}/config/${config_id}.sh"
+    echo running \"${CONFIG_SH}\"
     source "${CONFIG_SH}"
+    echo google_lat "${google_lat}"
     echo box_title- ${box_title} # from config file
-    "${EXEC_DIR}/capture_map.sh" '@${google_lat},${google_lng},${google_zoom}/data=!5m1!1e1' "${IMG_DIR}/${box_id}"
+    CAPTURE_URL="@${google_lat},${google_lng},${google_zoom}/data=!5m1!1e1"
+    CAPTURE_DIR="${IMG_DIR}/${box_id}"
+    echo CAPTURE_URL \"${CAPTURE_URL}\"
+    echo CAPTURE_DIR \"${CAPTURE_DIR}\"
+    "${EXEC_DIR}/capture_map.sh" ${CAPTURE_URL} ${CAPTURE_DIR}
+    #"${EXEC_DIR}/capture_map.sh" '@${google_lat},${google_lng},${google_zoom}/data=!5m1!1e1' "${IMG_DIR}/${box_id}"
 done
 
 #/home/ijl20/google_traffic/capture_map.sh  '@52.2122231,0.1196616,13z/data=!5m1!1e1' /data/tfc/google_traffic_map/      cambridge
