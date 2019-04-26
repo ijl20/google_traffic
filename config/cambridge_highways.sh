3cc7ccbad671a33f269f36892e24765da97825a7
Ian Lewis <ijl20@cam.ac.uk>
2019-04-26 09:13:37 +0100

    added cambridge_highways

---
+++ config/cambridge_highways.sh
@@ -0,0 +1,22 @@
+
+# Cambridge Highways
+
+# https://www.google.co.uk/maps/@52.1959833,-0.0024553,12z/data=!5m1!1e1
+
+box_title='Cambridge Highways'
+box_description='Highways around Cambridge, A14, A428, A1M, A11, A10'
+box_id='cambridge_highways'
+
+# coordinates for Google Maps URL
+google_lat=52.1960
+google_lng=-0.00246
+google_zoom='12z'
+
+# southwest and northeast box coordinates
+
+sw_lat=52.0823
+sw_lng=-0.3337
+
+ne_lat=52.3094
+ne_lng=0.3270
+
